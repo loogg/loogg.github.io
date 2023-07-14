fcc7428c2d75552b15dab2e3d1dec331f39b5a47
马龙伟 <2544047213@qq.com>
2023-07-14 23:13:39 +0800

    update action

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -8,14 +8,6 @@ export default navbar([
     prefix: "/agile_packages/",
     children: [
       { text: "关于", icon: "info", link: "" },
-      { text: "agile_upgrade", icon: "github", link: "https://github.com/loogg/agile_upgrade" },
-      { text: "agile_modbus", icon: "github", link: "https://github.com/loogg/agile_modbus" },
-      { text: "agile_led", icon: "github", link: "https://github.com/loogg/agile_led" },
-      { text: "agile_button", icon: "github", link: "https://github.com/loogg/agile_button" },
-      { text: "agile_console", icon: "github", link: "https://github.com/loogg/agile_console" },
-      { text: "agile_telnet", icon: "github", link: "https://github.com/loogg/agile_telnet" },
-      { text: "agile_jsmn", icon: "github", link: "https://github.com/loogg/agile_jsmn" },
-      { text: "agile_ftp", icon: "github", link: "https://github.com/loogg/agile_ftp" },
     ],
   },
   {
